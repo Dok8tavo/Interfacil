f4d01e2df3207be7140402820585f2caa999a453
Dok8tavo <vladicxo.pasxo@protonmail.ch>
2024-02-01 09:35:59 +0100

    Did an oopsie!

--- src/comparison.zig
+++ src/comparison.zig
@@ -1,2 +1,2 @@
-pub const Equivalent = @import("equivalent.zig").Equivalent;
-pub const Ordered = @import("ordered.zig").Ordered;
+pub const Equivalent = @import("comparison/equivalent.zig").Equivalent;
+pub const Ordered = @import("comparison/ordered.zig").Ordered;
