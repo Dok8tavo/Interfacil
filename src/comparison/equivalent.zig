35d15539f368bb958b3e5ce34a4b81af64698a23
Dok8tavo <vladicxo.pasxo@protonmail.ch>
2024-02-01 10:11:11 +0100

    Equality doesn't make sense for sum types

--- src/comparison/equivalent.zig
+++ src/comparison/equivalent.zig
@@ -258,8 +258,6 @@ pub fn Equivalent(comptime Contractor: type, comptime clauses: anytype) type {
 /// - reflexive `∀x : equals(x, x)`
 /// - symmetric `∀x, y : equals(x, y) == equals(y, x)`
 /// - transitive `∀x, y, z : (equals(x, y) and equals(y, z)) => equals(x, z)`
-/// It will be meaningful in most contexts, but if what you need is not an equality function, but a
-/// way to check for special equivalent cases, you should implement it yourself.
 pub fn equalsFn(comptime T: type) fn (T, T) bool {
     return struct {
         fn anyEquals(a: anytype, b: @TypeOf(a)) bool {
@@ -281,27 +279,20 @@ pub fn equalsFn(comptime T: type) fn (T, T) bool {
                     "The `{s}.anyEquals` function shouldn't compare floating point `{s}`!",
                     .{ @typeName(T), @typeName(A) },
                 ),
-                // The following are single-value types, therefore they allays equals themselves.
-                .Void, .Null, .Undefined => true,
-                // Error unions, optionals, and unions are all sum types. Two sum type instances
-                // are equals when they have the same tag and payload.
-                .ErrorUnion => if (a) |yes_a| {
-                    return if (b) |yes_b| anyEquals(yes_a, yes_b) else |_| false;
-                } else |no_a| {
-                    return if (b) |_| false else |no_b| no_a == no_b;
-                },
-                .Optional => if (a) |some_a| {
-                    return if (b) |some_b| anyEquals(some_a, some_b) else false;
-                } else b == null,
-                .Union => |Union| if (Union.tag_type) |Tag| {
-                    const variant_a = @field(Tag, @tagName(a));
-                    const variant_b = @field(Tag, @tagName(b));
-                    if (variant_a != variant_b) return false;
-                    const unwrapped_a = @field(a, @tagName(a));
-                    const unwrapped_b = @field(b, @tagName(b));
-                    return anyEquals(unwrapped_a, unwrapped_b);
-                } else misc.compileError(
-                    "Can't implement `{s}.anyEquals` function for the untagged union `{s}`!",
+                // Void is a single-value type.
+                .Void => true,
+                // `null` and `undefined` should not be comparable to anything, they're not
+                // representing data, or zero-sized data, but the absence of data.
+                .Null, .Undefined => misc.compileError(
+                    "The `{s}.anyEquals` function shouldn't compare `null` or `undefined`! " ++
+                        "Consider using partial equality instead.",
+                    .{@typeName(T)},
+                ),
+                // Comparing two items of different types doesn't make much sense, so partial
+                // equality is better suited in this case!
+                .ErrorUnion, .Optional, .Union => misc.compileError(
+                    "Can't implement `{s}.anyEquals` function for a type `{s}` which is a sum " ++
+                        "type! Consider using partial equality instead.",
                     .{ @typeName(T), @typeName(A) },
                 ),
                 // Structs, vectors and array are product types, two instances of them are equal if
@@ -318,31 +309,10 @@ pub fn equalsFn(comptime T: type) fn (T, T) bool {
                 // So we're effectively not comparing pointers, but the data they're pointing to.
                 .Pointer => |Pointer| switch (Pointer.size) {
                     .One => anyEquals(a.*, b.*),
-                    // Slices can be thought as a special case of both a pointer (we're comparing
-                    // the data they're pointing to), and arrays (we're comparing all their
-                    // members, except they don't allways have the same number of members). In both
-                    // cases this seems like the right thing to do.
-                    .Slice => if (a.len != b.len) false else for (a, b) |c, d| {
-                        if (!anyEquals(c, d)) break false;
-                    } else true,
-                    // A many pointer can emulate a slice if they have a sentinel.
-                    .Many => if (Pointer.sentinel) |sentinel_ptr| {
-                        var index: usize = 0;
-                        const sentinel = @as(*const Pointer.child, @ptrCast(sentinel_ptr)).*;
-                        return while (true) : (index += 1) {
-                            if (!anyEquals(a[index], b[index])) break false;
-                            if (anyEquals(a[index], sentinel)) break true;
-                        } else unreachable;
-                    } else misc.compileError(
-                        "Can't implement `{s}.anyEquals` for type `{s}`, which is a " ++ "many-item pointer to `{s}` without a sentinel!",
-                        .{ @typeName(T), @typeName(A), @typeName(Pointer.child) },
-                    ),
-                    // C-pointers don't carry enough information by themselves to know if they're
-                    // equals to one another.
-                    .C => misc.compileError(
-                        "Can't implement `{s}.anyEquals` function for type `{s}` which is a " ++
-                            "c-pointer to `{s}!",
-                        .{ @typeName(T), @typeName(A), @typeName(Pointer.child) },
+                    else => misc.compileError(
+                        "Can't implement `{s}.anyEquals` for type `{s}` which is a pointer to " ++
+                            "a varying number of items! Consider using partial equality instead.",
+                        .{ @typeName(T), @typeName(A) },
                     ),
                 },
                 // The following types, idk what to do, if there's anything to do.
