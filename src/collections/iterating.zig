55d71f055379004c3a910d65b365487e2a62e371
Dok8tavo <vladicxo.pasxo@protonmail.ch>
2024-02-12 10:53:45 +0100

    Some docs for iterating

--- src/collections/iterating.zig
+++ src/collections/iterating.zig
@@ -4,19 +4,92 @@ const contracts = @import("../contracts.zig");
 
 /// # Iterable
 ///
-/// TODO
+/// The `Iterable` interface is the base of the collections interfaces. It allows the user to
+/// iterate through the implementor, using a `next` function. It also lazily can be used as a map,
+/// a filter, or a reducer.
 ///
 /// ## Clauses
 ///
-/// TODO
+/// - `Self` is the type that will use the namespace returned by the interface. It shouldn't be a
+/// pointer. By default `Self` is the contractor.
+/// - `mutation` determine whether `VarSelf` is `*Self` or `Self`. By default `mutation` is
+/// `by_ref`.
+/// - `Item` is the return type of the items the iterator will return, it's required.
+/// - `curr: fn (Self) ?Item` is a function that returns the current item, it doesn't consume it
+/// nor should it mutate the iterable. It returns null when the iterable has been consumed.
+/// - `skip: fn (VarSelf) void` is a function that modify the iterable so that the `curr` function
+/// will return the next item.
 ///
 /// ## Declarations
 ///
-/// TODO
+/// 1. the current function `curr: fn (Self) ?Item`,
+/// 2. the filter function `filter: fn (*Self, *const fn (Item) bool) Filter(Item)`,
+/// 2. the map function `map: fn (*Self, To, *const fn (Item) To) Map(Item, To)`,
+/// 2. the next function `next: fn (VarSelf) ?Item`:
+///     - the multiple nexts function `nextTimes: fn (VarSelf, usize) ?Item`,
+/// 3. the reduce function `reduce: fn (*Self, *const fn (Item, Item) Item) Reduce(Item)`,
+/// 3. the skip function `skip: fn (VarSelf) void`:
+///     - the multiple skips function `skipTimes: fn (VarSelf, usize) void`,
+/// 4. the iterator function `asIterator: fn (*Self) Iterator(Item)`.
 ///
-/// ## Usage
+/// ### The current function `curr: fn (Self) ?Item`
 ///
-/// TODO
+/// This function returns the iterable currently "points" to. It's taken right from the clauses.
+///
+/// #### Usage
+///
+/// When an iterable is created, it should points to the first element:
+///
+/// ```zig
+/// const string = "Hello world!";
+/// var iteratable = StringIterable.from(string);
+/// assert(iterable.curr().? == 'H');
+/// ...
+/// ```
+///
+/// Once the `next`, `nextTimes`, `skip` or `skipTimes` function has been used, it should points to
+/// other elements:
+///
+/// ```zig
+/// ...
+/// const e = iterable.next();
+/// assert(iterable.curr().? == e);
+/// iterable.skip();
+/// assert(iterable.curr().? == 'l');
+/// iterable.skipTimes(2);
+/// assert(iterable.curr().? == 'o');
+/// const w = iterable.nextTimes(2);
+/// assert(iterable.curr().? == w);
+/// ```
+///
+/// Note that whenever `curr` is used right after `next` or `nextTimes`, their results are the
+/// equal to one another.
+///
+/// ### The skip function `skip: fn (self: VarSelf) void`
+///
+/// This function modifiy the state of the iterable so that it points to the very next item. If
+/// there's no next item, it'll point to null. If there's not even a current item, it won't do
+/// anything. It is equivalent to ignoring the result of the `next` function.
+///
+/// #### Usage
+///
+/// ```zig
+/// const array = [_]Enum{v1, v2, v3};
+/// var iterable = ArrayIterable(Enum).from(array);
+/// iterable.skip();
+/// assert(iterable.curr().? == v2);
+/// iterable.skip();
+/// assert(iterable.curr().? == v3);
+/// iterable.skip();
+/// assert(iterable.curr() == null);
+/// iterable.skip();
+/// assert(iterable.curr() == null);
+/// ```
+///
+/// #### The multiple skip function `skipTimes: fn (self: VarSelf, times: usize) void`
+///
+/// This function uses `skip` the given number of times, or until the `curr` function returns
+/// `null`.
 ///
 /// ## Testing
 ///
@@ -27,11 +100,12 @@ pub fn Iterable(comptime Contractor: type, comptime clauses: anytype) type {
     const Self: type = contract.Self;
     const VarSelf: type = contract.VarSelf;
     const Item = contract.require(.Item, type);
-
+    const skipClause = contract.require(.skip, fn (VarSelf) void);
     return struct {
+        /// This function returns the iterable currently "points" to.
         pub const curr = contract.require(.curr, fn (Self) ?Item);
-        pub const skip = contract.require(.skip, fn (VarSelf) void);
 
+        /// TODO
         pub fn next(self: VarSelf) ?Item {
             if (curr(contract.asSelf(self))) |item| {
                 defer skip(self);
@@ -39,15 +113,27 @@ pub fn Iterable(comptime Contractor: type, comptime clauses: anytype) type {
             } else return null;
         }
 
-        pub fn skipTimes(self: VarSelf, times: usize) void {
-            for (0..times) |_| if (next(self) == null) return null;
-        }
-
+        /// TODO
         pub fn nextTimes(self: VarSelf, times: usize) ?Item {
             skipTimes(self, times);
             return curr(self);
         }
 
+        /// This function modifiy the state of the iterable so that it points to the very next
+        /// item. If there's no next item, it'll point to `null`. If there's not even a current
+        /// item, it won't do anything. It is equivalent to ignoring the result of the `next`
+        /// function.
+        pub fn skip(self: VarSelf) void {
+            if (curr(self)) |_| skipClause(self);
+        }
+
+        /// This function uses `skip` the given number of times, or until the `curr` function
+        /// returns `null`.
+        pub fn skipTimes(self: VarSelf, times: usize) void {
+            for (0..times) |_| if (next(self) == null) return null;
+        }
+
+        /// TODO
         pub fn filter(self: *Self, condition: *const fn (Item) bool) Filter(Item) {
             return Filter(Item){
                 .iterator = asIterator(self),
@@ -55,6 +141,7 @@ pub fn Iterable(comptime Contractor: type, comptime clauses: anytype) type {
             };
         }
 
+        /// TODO
         pub fn reduce(self: *Self, operation: *const fn (Item, Item) Item) Reduce(Item) {
             return Reduce(Item){
                 .iterator = asIterator(self),
@@ -62,17 +149,19 @@ pub fn Iterable(comptime Contractor: type, comptime clauses: anytype) type {
             };
         }
 
+        /// TODO
         pub fn map(
             self: *Self,
             comptime To: type,
-            translator: *const fn (Item) To,
+            mapper: *const fn (Item) To,
         ) Map(Item, To) {
             return Map(Item, To){
                 .iterator = asIterator(self),
-                .translator = translator,
+                .translator = mapper,
             };
         }
 
+        /// TODO
         pub fn asIterator(self: *Self) Iterator(Item) {
             const Fn = struct {
                 pub fn skipFn(context: *anyopaque) void {
@@ -84,6 +173,7 @@ pub fn Iterable(comptime Contractor: type, comptime clauses: anytype) type {
                     return curr(ctx.*);
                 }
             };
+
             return Iterator(Item){
                 .ctx = self,
                 .vtable = .{
