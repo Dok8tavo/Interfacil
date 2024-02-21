0abc0b997e4ff8ea8360c30c1628d49929257a60
Dok8tavo <vladicxo.pasxo@protonmail.ch>
2024-02-21 08:21:46 +0100

    Commented out expamples until I can figure out how to deal with them.

--- build.zig
+++ build.zig
@@ -31,33 +31,33 @@ pub fn build(b: *std.Build) !void {
     doc_step.dependOn(&lib.step);
     doc_step.dependOn(&doc.step);
 
-    const examples_path = b.pathFromRoot("src/examples");
-    var examples_dir = try std.fs.openDirAbsolute(examples_path, .{ .iterate = true });
-    defer examples_dir.close();
-    var examples_walker = try examples_dir.walk(b.allocator);
-    while (try examples_walker.next()) |entry| switch (entry.kind) {
-        .file => {
-            const example_file = entry.basename;
-            if (!std.mem.endsWith(u8, example_file, ".zig")) continue;
-            const example_name = example_file[0 .. example_file.len - 4];
-            const example_path = std.fmt.allocPrint(
-                b.allocator,
-                "{s}/{s}",
-                .{ examples_path, entry.path },
-            ) catch @panic("OOM!");
-            const example = b.addExecutable(.{
-                .name = example_name,
-                .target = target,
-                .optimize = optimize,
-                .root_source_file = .{ .path = example_path },
-            });
-            addInterfacil(b, example, "interfacil");
-            const run_example = b.addRunArtifact(example);
-            const example_step = b.step(example_name, "Run the example!");
-            example_step.dependOn(&run_example.step);
-        },
-        else => continue,
-    };
+    //const examples_path = b.pathFromRoot("src/examples");
+    //var examples_dir = try std.fs.openDirAbsolute(examples_path, .{ .iterate = true });
+    //defer examples_dir.close();
+    //var examples_walker = try examples_dir.walk(b.allocator);
+    //while (try examples_walker.next()) |entry| switch (entry.kind) {
+    //    .file => {
+    //        const example_file = entry.basename;
+    //        if (!std.mem.endsWith(u8, example_file, ".zig")) continue;
+    //        const example_name = example_file[0 .. example_file.len - 4];
+    //        const example_path = std.fmt.allocPrint(
+    //            b.allocator,
+    //            "{s}/{s}",
+    //            .{ examples_path, entry.path },
+    //        ) catch @panic("OOM!");
+    //        const example = b.addExecutable(.{
+    //            .name = example_name,
+    //            .target = target,
+    //            .optimize = optimize,
+    //            .root_source_file = .{ .path = example_path },
+    //        });
+    //        addInterfacil(b, example, "interfacil");
+    //        const run_example = b.addRunArtifact(example);
+    //        const example_step = b.step(example_name, "Run the example!");
+    //        example_step.dependOn(&run_example.step);
+    //    },
+    //    else => continue,
+    //};
 }
 
 pub fn addInterfacil(b: *std.Build, to: *std.Build.Step.Compile, name: []const u8) void {
