b6aafaa69706325e8e789ab04eafc7063fe5b70c
Dok8tavo <vladicxo.pasxo@protonmail.ch>
2024-03-20 10:48:35 +0100

    Add a few examples for simple iteration

--- build.zig
+++ build.zig
@@ -1,4 +1,3 @@
-
 const std = @import("std");
 
 pub fn build(b: *std.Build) !void {
@@ -32,7 +31,45 @@ pub fn build(b: *std.Build) !void {
     doc_step.dependOn(&lib.step);
     doc_step.dependOn(&doc.step);
 
-    _ = b.addModule("interfacil", .{
+    const interfacil = b.addModule("interfacil", .{
         .root_source_file = .{ .path = "src/interfacil.zig" },
     });
+
+    const examples_path = b.pathFromRoot("src/examples/");
+    const examples_dir = try std.fs.openDirAbsolute(examples_path, .{ .iterate = true });
+    var walker = examples_dir.walk(b.allocator) catch @panic("OOM!");
+
+    while (walker.next() catch @panic("OOM!")) |entry| if (entry.kind == .file and std.mem.endsWith(u8, entry.basename, ".zig")) {
+        const name = entry.basename[0 .. entry.basename.len - ".zig".len];
+        const example = b.addExecutable(.{
+            .name = name,
+            .target = target,
+            .optimize = optimize,
+            .root_source_file = .{ .path = std.fmt.allocPrint(
+                b.allocator,
+                "{s}/{s}",
+                .{ examples_path, entry.path },
+            ) catch @panic("OOM!") },
+        });
+
+        example.root_module.addImport("interfacil", interfacil);
+
+        const run_example = b.addRunArtifact(example);
+        if (b.args) |args| run_example.addArgs(args);
+        const step = b.step(
+            std.fmt.allocPrint(
+                b.allocator,
+                "interfacil-example-{s}",
+                .{name},
+            ) catch @panic("OOM!"),
+            std.fmt.allocPrint(
+                b.allocator,
+                "Build & Run the interfacil example \"{s}\".",
+                .{name},
+            ) catch @panic("OOM!"),
+        );
+
+        step.dependOn(&run_example.step);
+        run_example.step.dependOn(&example.step);
+    };
 }
